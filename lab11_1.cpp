970ba12afb0855e1f4429d917142094aabd3c75c
Niphitpon Thosap <NiphitponT.63@cmu.ac.th>
2023-12-26 12:44:01 +0700

    ส่งงาน

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,27 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+#incloude<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+string grade ;
+int x;
+x= rand(time(0))%9;
+if(x==0){
+    cout<"You will get A in this 261102."}
+    else if 
+    
+
+cout<< “Press Enter 3 times to reveal your future.”;
+cin.getline()
+cin.getline()
+cin.getline()
+
+
+cout<< “Press Enter 3 times to reveal your future.”;
+}
